d87471305fdc5a30b088246b98c047a8913a73d7
SantiagoVS <santiagovs1402@gmail.com>
2024-11-26 23:28:23 -1100

    welcomepage arreglar

--- src/components/WelcomePage.js
+++ src/components/WelcomePage.js
@@ -27,7 +27,7 @@ const WelcomePage = () => {
             <Sidebar>
                 <MenuButton onClick={handleToggleServices}>
                     <FaBars size={20} color="#333" />
-                    <MenuText style={{color:'#333'}}>Servicios de Legalizado</MenuText>
+                    <MenuText>Servicios de Legalizado</MenuText>
                 </MenuButton>
                 {showServices && (
                     <Services>
@@ -40,12 +40,12 @@ const WelcomePage = () => {
             <MainContent expanded={showServices}>
                 <Logo src={mesy} alt="Logo" />
                 <Title>Sistema De Automatización Para La Legalización De Documentos</Title>
-                <Subtitle style={{color:'#333'}}>Bienvenido, {user.nombre} {user.apellido}</Subtitle>
+                <Subtitle>Bienvenido, {user.nombre} {user.apellido}</Subtitle>
                 <ProfileContainer>
-                    <ProfileName style={{color:'#333'}}>{user.nombre} {user.apellido}</ProfileName>
-                    <ProfileButton style={{color:'#333'}} onClick={handleToggleProfileMenu}>▼</ProfileButton>
+                    <ProfileName>{user.nombre} {user.apellido}</ProfileName>
+                    <ProfileButton onClick={handleToggleProfileMenu}>▼</ProfileButton>
                     {showProfileMenu && (
-                        <ProfileMenu >
+                        <ProfileMenu>
                             <ProfileMenuButton onClick={handleViewProfile}>Ver Perfil</ProfileMenuButton>
                             <ProfileMenuButton onClick={handleLogout}>Cerrar Sesión</ProfileMenuButton>
                         </ProfileMenu>
@@ -60,23 +60,25 @@ const Container = styled.div`
     display: flex;
     height: 100vh;
     background-color: #f0f4f8;
-    color: #fff;
+    color: #333;
     font-family: Arial, sans-serif;
-
+    flex-direction: column;
+    
     @media (min-width: 768px) {
         flex-direction: row;
     }
-`;  
+`;
 
 const Sidebar = styled.div`
-    width: 200px;
+    width: 100%;
     padding: 10px;
     background-color: #d8dddf;
     display: flex;
     flex-direction: column;
     align-items: center;
     transition: width 0.3s;
-@media (min-width: 768px) {
+    
+    @media (min-width: 768px) {
         width: 200px;
     }
 `;
@@ -92,7 +94,7 @@ const MenuButton = styled.button`
 
 const MenuText = styled.span`
     margin-left: 10px;
-    color: #fff;
+    color: #333;
     font-size: 14px;
 `;
 
@@ -104,38 +106,44 @@ const MainContent = styled.div`
     align-items: flex-start;
     justify-content: center;
     transition: margin-left 0.3s;
+    
     ${({ expanded }) => expanded && `margin-left: 200px;`}
-@media (min-width: 768px) {
+    
+    @media (min-width: 768px) {
         align-items: flex-start;
         text-align: left;
     }
 `;
 
 const Logo = styled.img`
-    width: 193px;
-    height: 193px;
+    width: 50%;
+    height: auto;
     margin-bottom: 20px;
-@media (min-width: 768px) {
+    max-width: 193px;
+    
+    @media (min-width: 768px) {
         width: 193px;
         height: 193px;
     }
 `;
 
 const Title = styled.h1`
-    font-size: 36px;
+    font-size: 28px;
     font-weight: bold;
     margin-bottom: 20px;
     color: #39a4cb;
-@media (min-width: 768px) {
-        font-size: 30px;
+    
+    @media (min-width: 768px) {
+        font-size: 36px;
     }
 `;
 
 const Subtitle = styled.h2`
-    font-size: 24px;
+    font-size: 18px;
     margin-bottom: 20px;
-@media (min-width: 768px) {
-        font-size: 22px;
+    
+    @media (min-width: 768px) {
+        font-size: 24px;
     }
 `;
 
@@ -154,19 +162,22 @@ const ServiceButton = styled.button`
     border-radius: 5px;
     cursor: pointer;
     transition: background-color 0.3s ease;
+    
     &:hover {
         background-color: #555;
     }
 `;
 
 const ProfileContainer = styled.div`
-    position: absolute;
+    position: relative;
     top: 10px;
     right: 10px;
     z-index: 1000;
     display: flex;
     align-items: center;
-@media (min-width: 768px) {
+    justify-content: flex-end;
+    
+    @media (min-width: 768px) {
         position: absolute;
         top: 10px;
         right: 10px;
@@ -175,10 +186,10 @@ const ProfileContainer = styled.div`
 
 const ProfileName = styled.span`
     margin-right: 10px;
-    font-size: 18px;
-    color: #fff;
- @media (min-width: 768px) {
-        margin-right: 10px;
+    font-size: 16px;
+    
+    @media (min-width: 768px) {
+        font-size: 18px;
     }
 `;
 
@@ -187,7 +198,7 @@ const ProfileButton = styled.button`
     border: none;
     cursor: pointer;
     font-size: 16px;
-    color: #fff;
+    color: #333;
 `;
 
 const ProfileMenu = styled.div`
@@ -198,7 +209,8 @@ const ProfileMenu = styled.div`
     border-radius: 5px;
     padding: 10px 0;
     z-index: 1000;
-@media (min-width: 768px) {
+    
+    @media (min-width: 768px) {
         position: absolute;
         top: 100%;
         right: 0;
@@ -215,9 +227,10 @@ const ProfileMenuButton = styled.button`
     cursor: pointer;
     width: 100%;
     text-align: left;
+    
     &:hover {
         background-color: #444;
-        color: #fff
+        color: #fff;
     }
 `;
 
