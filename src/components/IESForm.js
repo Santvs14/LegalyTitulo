7cafb6fc2583e9b8bcbe01586d57e57fed2de236
SantiagoVS <santiagovs1402@gmail.com>
2024-11-27 10:44:36 -1100

    IESForm ajuste pantalla movil5.7

--- src/components/IESForm.js
+++ src/components/IESForm.js
@@ -279,18 +279,18 @@ const FormWrapper = styled.div`
 
 
 const Form = styled.form`
-  width: 93%;
-  padding: 1.5rem;
+  width: 95%;
+  padding: 1rem;
   background-color: #ffffff;
   border-radius: 8px;
   border: 1px solid #e0e0e0;
   box-shadow: 0 4px 6px rgba(0, 0, 0, 0.1);
-            height: 83vh; /* Usar la altura de la ventana */
+            height: 85vh; /* Usar la altura de la ventana */
 
       @media (min-width: 768px) {
   width: 90%;
     padding: 2rem;
-          height: 80vh; /* Usar la altura de la ventana */
+          height: 77vh; /* Usar la altura de la ventana */
 
 
     }
